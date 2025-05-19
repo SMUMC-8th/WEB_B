4da6842bb0cd88f67eaef6914d0e8782e452e638
SEBINorSEBOUT <sebin030710@gmail.com>
2025-05-19 20:37:16 +0900

    FEAT: github연결

--- src/Page/ClubIntro/components/footer.tsx
+++ src/Page/ClubIntro/components/footer.tsx
@@ -1,8 +1,7 @@
 // const Footer: React.FC = () => {
 //     return (
-//         <div className="w-full h-[100px] bg-white text-black flex justify-center items-center ">
-//             <img src={'/footer.svg'} alt="Logo" className="w-auto h-auto" />{' '}
-//             {/* 로고 */}
+//         <div className="absolute left-[850px] bottom-[50px] justify-center w-[300px] h-[150px] ">
+//             <img src={'/footer.svg'} alt="Logo" className="w-auto h-auto" />
 //         </div>
 //     );
 // };
@@ -11,8 +10,14 @@
 
 const Footer: React.FC = () => {
     return (
-        <div className="absolute left-[850px] bottom-[50px] justify-center w-[300px] h-[150px] ">
-            <img src={'/footer.svg'} alt="Logo" className="w-auto h-auto" />
+        <div className="absolute left-[850px] bottom-[50px] justify-center w-[300px] h-[150px]">
+            <a
+                href="https://github.com/SMUMC-8th"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                <img src="/footer.svg" alt="Logo" className="w-auto h-auto" />
+            </a>
         </div>
     );
 };
