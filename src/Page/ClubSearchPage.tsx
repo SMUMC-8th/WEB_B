90aaff4c356627d5be025f259e95afbfda6f7e6a
jeha0714 <whrydus0714@gmail.com>
2025-05-20 17:32:08 +0900

    FEAT: Club Search Page

--- src/Page/ClubSearchPage.tsx
+++ src/Page/ClubSearchPage.tsx
@@ -1,38 +1,37 @@
 // ClubListPage.tsx
-import Tags from '../components/result/Tags';
+import Tags from "../components/result/Tags";
 // import Card from '../components/ClubList/Card';
-import NoResult from '../components/result/NoResult';
-import SearchBar from '../components/ClubList/SearchBar';
-import Footer from '../components/Footer';
-import SideBar from '../components/SideBar';
-import { useState } from 'react';
+import NoResult from "../components/result/NoResult";
+import SearchBar from "../components/ClubList/SearchBar";
+import SideBar from "../components/SideBar";
+import { useState } from "react";
 
 const ClubSearchPage = () => {
-    const [searchKeyword, setSearchKeyword] = useState('');
+  const [searchKeyword, setSearchKeyword] = useState("");
 
-    // 샘플 데이터
-    const clubData = [
-        // 실제로는 검색 결과 기준 필터링된 결과여야 함
-    ];
+  // 샘플 데이터
+  const clubData = [
+    // 실제로는 검색 결과 기준 필터링된 결과여야 함
+  ];
 
-    return (
-        <>
-            <SearchBar categoryTitle="" />
+  return (
+    <>
+      <SearchBar categoryTitle="" />
 
-            <div className="flex flex-col lg:flex-row">
-                <SideBar />
-                <div className="flex-1 px-6">
-                    {clubData.length > 0 ? (
-                        <Tags data={clubData} />
-                    ) : (
-                        <NoResult keyword={searchKeyword} />
-                    )}
-                </div>
-            </div>
-
-            <Footer />
-        </>
-    );
+      <div className="flex flex-col lg:flex-row">
+        <div className="hidden lg:block w-full lg:w-[200px]">
+          <SideBar />
+        </div>
+        <div className="flex-1 px-6">
+          {clubData.length > 0 ? (
+            <Tags data={clubData} />
+          ) : (
+            <NoResult keyword={searchKeyword} />
+          )}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default ClubSearchPage;
