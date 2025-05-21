7d75a609e255af15516fe2a98c19bf2347b6c19f
jeha0714 <whrydus0714@gmail.com>
2025-05-21 18:36:19 +0900

    ADD: axios base

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,7 +1,14 @@
 import axios from "axios";
 import { TMatchForm, TMatchResult } from "../types/MatchForm";
 
-export const fetchMatchedClubs=async(form:TMatchForm):Promise<TMatchResult[]>=>{
-    const response = await axios.post('ddddd', form);
-    return response.data.result;
+export const fetchMatchedClubs = async (
+  form: TMatchForm
+): Promise<TMatchResult[]> => {
+  const response = await axios.post("ddddd", form);
+  return response.data.result;
 };
+
+export const umcServerNoAuth = axios.create({
+  baseURL: `${import.meta.env.VITE_SERVER}`,
+  timeout: 1000,
+});
