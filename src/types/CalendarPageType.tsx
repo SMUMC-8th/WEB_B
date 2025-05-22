7ccbf3bc8e69dcf20101eec0d884a42a459f7647
jeha0714 <whrydus0714@gmail.com>
2025-05-22 11:35:27 +0900

    Fix: merge conflict with main

--- src/types/CalendarPageType.tsx
+++ src/types/CalendarPageType.tsx
@@ -1,17 +1,10 @@
-type ClubSchedule = {
+export type ClubSchedule = {
   club_id: number;
   name: string;
   firstStart: string;
   firstEnd: string;
   secondStart: string;
   secondEnd: string;
-};
-
-export type ApiCalendarResponse = {
-  success: boolean;
-  code: string;
-  message: string;
-  result: ClubSchedule;
 };
 
 export type CalendarEvent = {
